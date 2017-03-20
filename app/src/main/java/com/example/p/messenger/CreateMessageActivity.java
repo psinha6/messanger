784e7edc118313503ea90339304df0ff753e7207
vikasmca2k11 <psinha6@gmail.com>
2017-03-20 23:51:45 +0530

    Messanger HeadFirst Demo Codes

---
+++ app/src/main/java/com/example/p/messenger/CreateMessageActivity.java
@@ -0,0 +1,24 @@
+package com.example.p.messenger;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class CreateMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_message);
+    }
+
+    public void onSendMessage(View view){
+        EditText messageView = (EditText) findViewById(R.id.message);
+        String messageText = messageView.getText().toString();
+        Intent intent = new Intent(this, RecieveMessageActivity.class);
+        intent.putExtra(RecieveMessageActivity.EXTRA_MESSAGE, messageText);
+        startActivity(intent);
+    }
+}
